475348b4055986f334064df036d0e0fcfedf463c
GDoubleSix <muqigewa84@gmail.com>
2024-05-31 06:41:45 +0700

    solving eror

--- resources/views/partials/section.blade.php
+++ resources/views/partials/section.blade.php
@@ -17,33 +17,34 @@
           <div class="property-slider-wrap">
             <div class="property-slider">
               @foreach ($property as $property)
-                <div class="property-item">
-                  <a href="property-single.html" class="img">
-                    <img src="{{ $property->picture }}" alt="Image" class="img-fluid" />
-                  </a>
+              <div class="property-item">
+                <a href="property-single.html" class="img">
+                  <img src="{{$property->picture}}" alt="Image" class="img-fluid" />
+                </a>
 
-                  <div class="property-content">
-                    <div class="price mb-2"><span>$ {{$property->price}} </span></div>
-                    <div>
-                      <span class="d-block mb-2 text-black-50">{{$property->name}}</span>
-                      <span class="city d-block mb-3"> {{$property->addres}} </span>
+                <div class="property-content">
+                  <div class="price mb-2"><span>${{$property->price}}</span></div>
+                  <div>
+                    <span class="d-block mb-2 text-black-50">{{$property->name}}</span>
+                    <span class="city d-block mb-3">{{$property->address}}</span>
 
-                      <div class="specs d-flex mb-4">
-                        <span class="d-block d-flex align-items-center me-3">
-                          <span class="icon-bed me-2"></span>
-                          <span class="caption"> {{$property->num_of_bed}} </span>
-                        </span>
-                        <span class="d-block d-flex align-items-center">
-                          <span class="icon-bath me-2"></span>
-                          <span class="caption">{{ (string) $property->$num_of_bath}}</span>
-                        </span>
-                      </div>
-
-                      <a href="property-single.html" class="btn btn-primary py-2 px-3">See details</a>
+                    <div class="specs d-flex mb-4">
+                      <span class="d-block d-flex align-items-center me-3">
+                        <span class="icon-bed me-2"></span>
+                        <span class="caption">{{$property->num_of_bed}}</span>
+                      </span>
+                      <span class="d-block d-flex align-items-center">
+                        <span class="icon-bath me-2"></span>
+                        <span class="caption">{{$property->num_of_bath}}</span>
+                      </span>
                     </div>
+
+                    <a href="property-single.html" class="btn btn-primary py-2 px-3">See details</a>
                   </div>
-              @endforeach
+                </div>
               </div>
+              @endforeach
+
               <!-- .item -->
 
             <div id="property-nav" class="controls" tabindex="0" aria-label="Carousel Navigation">
